5d5ed03727fa2776cab6857070ee37ee7a271a3b
abrenycz <abrenycz@gmail.com>
2018-04-21 22:06:07 -0500

    added api request

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [ 
-	{ 
-		title: 'React',
-		url: 'https://facebook.github.io/react/',
-		author: 'Jordan Walke', 
-	    num_comments: 3, 
-	    points: 4, 
-	    objectID: 0, 
-	}, 
+const DEFAULT_QUERY = 'redux';
 
-	{ 
-		title: 'Redux',
-		url: 'https://github.com/reactjs/redux', 
-		author: 'Dan Abramov, Andrew Clark',
-		num_comments: 2,
-		points: 5,
-		objectID: 1, 
-	},
-]
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
+console.log(url); 
 
 const date_ComputedPropertyNameTest = 'date';
 
@@ -30,15 +20,29 @@ const isSearched = searchTerm => item => item.title.toLowerCase().includes(searc
 class App extends Component { 
 	constructor(props){
 		super(props);
+
 		this.state = {
-			list,
-			searchTerm: '',
-			[date_ComputedPropertyNameTest]: 25,
+			result: null,
+			searchTerm: DEFAULT_QUERY,
 		};
 
+		this.setSearchTopStories = this.setSearchTopStories.bind(this);
 		this.onDismiss = this.onDismiss.bind(this);
 		this.onSearchChange = this.onSearchChange.bind(this);
 
+	}
+
+	setSearchTopStories(result) {
+		this.setState({ result });
+	}
+
+	componentDidMount() {
+		const { searchTerm } = this.state;
+
+		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+		.then(response => response.json())
+		.then(result => this.setSearchTopStories(result))
+		.catch(error =>error);
 	}
 
 	onDismiss(id){
@@ -50,7 +54,10 @@ class App extends Component {
 	}
 
 	render() { 
-		const { list, searchTerm } = this.state; // ES6 deconstructing
+		const { searchTerm, result } = this.state; // ES6 deconstructing
+		if(!result) { return null; }
+
+		console.log(this.state);
 		return ( 
 			<div className="page">
 				<div className="interactions"> 
@@ -60,7 +67,7 @@ class App extends Component {
 					/>
 				</div>
 				<Table
-					list={list}
+					list={result.hits}
 					pattern={searchTerm}
 					onDismiss={this.onDismiss}
 				/>
