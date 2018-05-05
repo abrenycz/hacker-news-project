02aa3674fb1964854209e6cf17750a243d2bb11b
abrenycz <abrenycz@gmail.com>
2018-05-04 20:03:56 -0500

    server side searching added

--- src/App.js
+++ src/App.js
@@ -29,6 +29,8 @@ class App extends Component {
 		this.setSearchTopStories = this.setSearchTopStories.bind(this);
 		this.onDismiss = this.onDismiss.bind(this);
 		this.onSearchChange = this.onSearchChange.bind(this);
+		this.onSearchSubmit = this.onSearchSubmit.bind(this);
+		this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
 
 	}
 
@@ -39,18 +41,33 @@ class App extends Component {
 	componentDidMount() {
 		const { searchTerm } = this.state;
 
-		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-		.then(response => response.json())
-		.then(result => this.setSearchTopStories(result))
-		.catch(error =>error);
+		this.fetchSearchTopStories(searchTerm);
 	}
 
 	onDismiss(id){
-			const isNotId = item => item.objectID !== id; const updatedList = this.state.list.filter(isNotId); this.setState({ list: updatedList });
+			const isNotId = item => item.objectID !== id; 
+			const updatedHits = this.state.result.hits.filter(isNotId); 
+			this.setState({ 
+				result: {...this.state.result, hits: updatedHits }
+			});
+			
 	}
 
 	onSearchChange(event){
 		this.setState({searchTerm: event.target.value});
+	}
+
+	onSearchSubmit(event) {
+		const { searchTerm } = this.state;
+		this.fetchSearchTopStories(searchTerm);
+		event.preventDefault();
+	}
+
+	fetchSearchTopStories(searchTerm) {
+		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+		.then(response => response.json())
+		.then(result => this.setSearchTopStories(result))
+		.catch(error =>error);
 	}
 
 	render() { 
@@ -64,13 +81,17 @@ class App extends Component {
 					<Search
 						value={searchTerm} 
 						onChange={this.onSearchChange}
-					/>
+						onSubmit={this.onSearchSubmit}
+					>
+					Search
+					</Search>
 				</div>
+				{ result &&
 				<Table
 					list={result.hits}
-					pattern={searchTerm}
 					onDismiss={this.onDismiss}
 				/>
+				}
 			</div> 
 		);
 	}
@@ -87,7 +108,7 @@ const Button = ({onClick, className = '', children}) =>
 
 const Table = ({list, pattern, onDismiss}) =>
 		<div className="table"> 
-			{list.filter(isSearched(pattern)).map(item => 
+			{list.map(item => 
 				<div key={item.objectID} className = "table-row"> 
 				<span style={{ width: '40%' }}> 
 					<a href={item.url}>{item.title}</a> 
@@ -105,13 +126,16 @@ const Table = ({list, pattern, onDismiss}) =>
 			)} 
 		</div>
 
-const Search = ({value, onChange, children}) => 
-	<form>
-		{children} 
+const Search = ({value, onChange, onSubmit, children}) => 
+	<form onSubmit={onSubmit}>
 		<input 
 		type="text" 
 		value={value} 
 		onChange={onChange} />
+		<button
+		type="submit">
+		{children}
+		</button>
 	</form> 
 
 export default App;
